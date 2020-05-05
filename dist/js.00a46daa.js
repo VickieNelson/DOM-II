61cd9e3701018078c7d90f7cce1fbc11e38c040d
Vickie Nelson <vickie.isabel.nelson@gmail.com>
2020-05-05 14:32:11 -0500

    mouse enter, mouse leave, dbl click, mousedown

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -119,6 +119,26 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   return newRequire;
 })({"js/index.js":[function(require,module,exports) {
 // Your code goes here
+// Change the text color to crimson on mouse enter
+var footerText = document.querySelector(".footer p");
+footerText.addEventListener("mouseenter", function () {
+  footerText.style.color = "crimson";
+}); // Change the color to grey on mouse leave
+
+var footer = document.querySelector(".footer");
+footer.addEventListener("mouseleave", function () {
+  footer.style.background = "grey";
+}); // click mouse down to change width to 200px
+
+var photo = document.querySelector(".img-content img");
+photo.addEventListener("mousedown", function () {
+  photo.style.width = "200px";
+}); // Double clikc to change display to none
+
+var intro = document.querySelector(".intro img");
+intro.addEventListener("dblclick", function () {
+  intro.style.display = "none";
+});
 },{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -147,7 +167,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50389" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50670" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 // Your code goes here
+
+// Change the text color to crimson on mouse enter
+const footerText = document.querySelector(".footer p");
+footerText.addEventListener("mouseenter", function () {
+  footerText.style.color = "crimson";
+});
+
+// Change the color to grey on mouse leave
+const footer = document.querySelector(".footer");
+footer.addEventListener("mouseleave", function () {
+  footer.style.background = "grey";
+});
+
+// click mouse down to change width to 200px
+const photo = document.querySelector(".img-content img");
+photo.addEventListener("mousedown", function () {
+  photo.style.width = "200px";
+});
+
+// Double clikc to change display to none
+const intro = document.querySelector(".intro img");
+intro.addEventListener("dblclick", function () {
+  intro.style.display = "none";
+});
